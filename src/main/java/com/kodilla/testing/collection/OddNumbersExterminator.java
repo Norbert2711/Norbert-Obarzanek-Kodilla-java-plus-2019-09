c8dbe8d64305cffa6b46ba98eca73090b5a74176
Norbert2711 <norbertu2711@gmail.com>
2019-11-07 19:37:47 +0100

    First Commit

---
+++ src/main/java/com/kodilla/testing/collection/OddNumbersExterminator.java
@@ -0,0 +1,19 @@
+package com.kodilla.testing.collection;
+import java.util.ArrayList;
+
+public class OddNumbersExterminator {
+
+    public ArrayList<Integer> exterminator(ArrayList<Integer> numbers) {
+
+        ArrayList<Integer> parzyste = new ArrayList<>();
+        for (Integer liczby : numbers) {
+            if (liczby % 2 == 0) {
+                parzyste.add(liczby);
+            }
+        }
+        return parzyste;
+    }
+    public boolean isEmpty() {
+        return true;
+    }
+}
---
+++ src/main/java/com/kodilla/testing/collection/TestMain.java
@@ -0,0 +1,7 @@
+package com.kodilla.testing.collection;
+
+public class TestMain {
+    public static void main(String[] args) {
+
+    }
+}
