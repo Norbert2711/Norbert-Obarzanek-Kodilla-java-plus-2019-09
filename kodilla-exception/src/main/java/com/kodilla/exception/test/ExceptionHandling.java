f3dc2c6b44ac265b7e9f1441a81327dcc3c0c394
Norbert2711 <norbertu2711@gmail.com>
2019-12-14 20:51:14 +0100

    Second Challange try, catch, finally

--- kodilla-exception/src/main/java/com/kodilla/exception/test/ExceptionHandling.java
+++ kodilla-exception/src/main/java/com/kodilla/exception/test/ExceptionHandling.java
@@ -8,10 +8,15 @@ public class ExceptionHandling {
     public static void main(String[] args) {
 
         SecondChallenge secondChallenge = new SecondChallenge();
+        String result ="RESULT:";
         try {
-            secondChallenge.probablyIWillThrowException(2.2, 1.5);
+             result = secondChallenge.probablyIWillThrowException(2.1, 1.5);
+
         } catch (Exception e) {
             System.out.println("We have problem! " + e);
+
+        } finally {
+            System.out.println(result);
         }
     }
 }
