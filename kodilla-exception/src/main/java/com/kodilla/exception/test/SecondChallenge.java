49b58ba7c61f11e129bc2b5955281511a16a9824
Norbert2711 <norbertu2711@gmail.com>
2019-12-14 15:35:42 +0100

    SecondChallenge final version try, catch

--- kodilla-exception/src/main/java/com/kodilla/exception/test/SecondChallenge.java
+++ kodilla-exception/src/main/java/com/kodilla/exception/test/SecondChallenge.java
@@ -3,15 +3,10 @@ package com.kodilla.exception.test;
 public class SecondChallenge {
 
     public String probablyIWillThrowException(double x, double y) throws Exception {
-        try {
+
             if (x >= 2 || x < 1 || y == 1.5) {
             }
             return "Done!";
 
-        } catch (Exception e) {
-            throw new Exception();
-        } finally {
-            System.out.println("Still working! ");
-        }
     }
 }
