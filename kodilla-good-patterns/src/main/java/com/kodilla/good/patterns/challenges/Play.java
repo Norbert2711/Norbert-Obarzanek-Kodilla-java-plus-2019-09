99c36eb4f788fdef54cdfa581b19786e9d05bcad
Norbert2711 <norbertu2711@gmail.com>
2020-01-11 11:51:53 +0100

    Stream

---
+++ kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/challenges/Play.java
@@ -0,0 +1,21 @@
+package com.kodilla.good.patterns.challenges;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public class Play {
+    public static void main(String[] args) {
+        MovieStore movieStore = new MovieStore();
+
+        Map<String, List<String>> mapWithWovies = movieStore.getMovies();
+
+        String result = mapWithWovies.entrySet()
+                .stream()
+                .flatMap(entry -> entry.getValue().stream())
+                .collect(Collectors.joining(" ! "));
+        System.out.println(result);
+
+    }
+}
