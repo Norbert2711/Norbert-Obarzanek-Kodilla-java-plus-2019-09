1b6d74dc2a70acda13108c6f19b0f69a1434208f
Norbert2711 <norbertu2711@gmail.com>
2020-01-24 19:14:37 +0100

    First test with Spring and Beans

---
+++ kodilla-spring/src/main/java/com/kodilla/spring/forum/ForumUser.java
@@ -0,0 +1,36 @@
+package com.kodilla.spring.forum;
+
+import org.springframework.stereotype.Component;
+
+import java.util.Objects;
+
+@Component
+public class ForumUser {
+
+    private String username;
+
+    public ForumUser() {
+        this.username = "John Smith";
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        ForumUser forumUser = (ForumUser) o;
+        return Objects.equals(username, forumUser.username);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(username);
+    }
+}
