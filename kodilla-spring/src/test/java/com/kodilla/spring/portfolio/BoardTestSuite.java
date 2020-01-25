dc68dd6f63d3be98abbd4c3cbb678f9119bd9409
Norbert2711 <norbertu2711@gmail.com>
2020-01-25 20:46:38 +0100

    BoardTest Beans like beans

--- kodilla-spring/src/test/java/com/kodilla/spring/portfolio/BoardTestSuite.java
+++ kodilla-spring/src/test/java/com/kodilla/spring/portfolio/BoardTestSuite.java
@@ -1,12 +1,8 @@
 package com.kodilla.spring.portfolio;
-
 import org.junit.Assert;
 import org.junit.Test;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-
-import java.util.Arrays;
-
 public class BoardTestSuite {
 
     @Test
