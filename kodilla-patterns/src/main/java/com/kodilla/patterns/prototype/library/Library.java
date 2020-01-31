aeb738bfe3dfde228ee6b48a6564556a08b219be
Norbert2711 <norbertu2711@gmail.com>
2020-01-31 22:10:32 +0100

    Prototype, Deep and Shallow copy task

---
+++ kodilla-patterns/src/main/java/com/kodilla/patterns/prototype/library/Library.java
@@ -0,0 +1,57 @@
+package com.kodilla.patterns.prototype.library;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public final class Library implements Cloneable {
+
+    private String name;
+    private Set<Book> books = new HashSet<>();
+
+    public Library(String name) {
+        this.name = name;
+    }
+
+    public void addBook(Book book) {
+        books.add(book);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setBooks(Set<Book> books) {
+        this.books = books;
+    }
+
+    public Set<Book> getBooks() {
+        return books;
+    }
+
+    @Override
+    public String toString() {
+        return "Library{" +
+                "name='" + name + '\'' +
+                ", books=" + books +
+                '}';
+    }
+
+    public Library shallowCopy() throws CloneNotSupportedException {
+        return (Library) super.clone();
+    }
+
+    public Library deepCopy() throws CloneNotSupportedException {
+        Library clonedLibrary = (Library) super.clone();
+        clonedLibrary.setBooks(new HashSet<>());
+
+        for (Book book : getBooks()) {
+            clonedLibrary.addBook(new Book(book.getTitle(),book.getAuthor(),book.getPublicationDate()));
+        }
+        return clonedLibrary;
+    }
+
+}
---
+++ kodilla-patterns/src/test/java/com/kodilla/patterns/prototype/library/LibraryTestSuite.java
@@ -0,0 +1,53 @@
+package com.kodilla.patterns.prototype.library;
+import org.junit.Assert;
+import org.junit.Test;
+import java.time.LocalDate;
+public class LibraryTestSuite {
+
+    @Test
+    public void testGetBooks() {
+
+        //Given
+        Library library = new Library("BIG Library");
+
+        Book book1 = new Book("Dzieci z Bullerbyn", "Astrid Lindgren",
+                LocalDate.of(1947, 2, 4));
+        Book book2 = new Book("Pinokio", "Carlo Collodi",
+                LocalDate.of(1883, 12, 8));
+        Book book3 = new Book("Slepnac od swiatel", "Jakub Zulczyk",
+                LocalDate.of(2014, 10, 5));
+        Book book4 = new Book("ZLE PSY. W IMIE ZASAD", "Patryk Vega",
+                LocalDate.of(2015, 1, 30));
+
+        library.addBook(book1);
+        library.addBook(book2);
+        library.addBook(book3);
+        library.addBook(book4);
+
+        //shallow copy
+        Library clonedLibrary = null;
+        try {
+            clonedLibrary = library.shallowCopy();
+            clonedLibrary.setName("Shallow copy BIG Library");
+        } catch (CloneNotSupportedException e) {
+            System.out.println(e);
+        }
+
+        //deep copy
+        Library deepClonedLibrary = null;
+        try {
+            deepClonedLibrary = library.deepCopy();
+            deepClonedLibrary.setName("Deep copy BIG Library");
+        } catch (CloneNotSupportedException e) {
+            System.out.println(e);
+        }
+
+        //When
+        //Then
+        System.out.println(clonedLibrary);
+        System.out.println(deepClonedLibrary);
+        Assert.assertEquals(4, clonedLibrary.getBooks().size());
+        Assert.assertEquals(4, deepClonedLibrary.getBooks().size());
+
+    }
+}
