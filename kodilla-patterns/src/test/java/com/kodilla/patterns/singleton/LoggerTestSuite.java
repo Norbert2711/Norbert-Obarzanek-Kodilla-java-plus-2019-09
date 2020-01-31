da0b994ee5ecef62bf0efa728e8fcd8c7dc99505
Norbert2711 <norbertu2711@gmail.com>
2020-01-31 20:21:43 +0100

    First example with Singleton

--- kodilla-patterns/src/test/java/com/kodilla/patterns/singleton/LoggerTestSuite.java
+++ kodilla-patterns/src/test/java/com/kodilla/patterns/singleton/LoggerTestSuite.java
@@ -7,11 +7,14 @@ public class LoggerTestSuite {
 
     @Test
     public void getLastLogTest() {
-        //Given
-        //When
-        String log1 = Logger.getInstance().getLastLog();
 
+        //Given
+        String log1 = "First log";
+        String log2 = "Second log";
+        //When
+        Logger.getInstance().log(log1);
         //Then
-        Assert.assertEquals(Logger.getInstance().getLastLog(), log1);
+        Assert.assertEquals(log1, Logger.getInstance().getLastLog());
+
     }
 }
