833d70f7e3023551d38846b48153e028f4afc13c
Norbert2711 <norbertu2711@gmail.com>
2020-02-04 20:28:53 +0100

    Task with spring example

--- kodilla-spring/src/main/java/com/kodilla/spring/portfolio/BoardConfig.java
+++ kodilla-spring/src/main/java/com/kodilla/spring/portfolio/BoardConfig.java
@@ -1,28 +1,41 @@
 package com.kodilla.spring.portfolio;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class BoardConfig {
 
-    public BoardConfig() {
+    @Autowired
+    @Qualifier("List tasks to Do")
+    TaskList toDoList;
 
-    }
+    @Autowired
+    @Qualifier("List tasks in progress")
+    TaskList inProgressList;
+
+    @Autowired
+    @Qualifier("List tasks done")
+    TaskList doneList;
 
     @Bean
     public Board getBoard() {
-        return new Board(getToDoList(), getInProgressList(), getDoneList());
+        return new Board(toDoList, inProgressList, doneList);
     }
-    @Bean
+
+    @Bean(name = "List tasks to Do")
     TaskList getToDoList() {
         return new TaskList();
     }
-    @Bean
+
+    @Bean(name = "List tasks in progress")
     TaskList getInProgressList() {
         return new TaskList();
     }
-    @Bean
+
+    @Bean(name = "List tasks done")
     TaskList getDoneList() {
         return new TaskList();
     }
