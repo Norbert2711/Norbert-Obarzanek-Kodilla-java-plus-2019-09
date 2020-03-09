8c9ee5710900cdd6cfc46c382afd7b51961e6b7e
Norbert2711 <norbertu2711@gmail.com>
2020-03-09 17:58:41 +0100

    First commit REST-API

---
+++ tasks/src/main/java/com/crud/tasks/domain/Task.java
@@ -0,0 +1,11 @@
+package com.crud.tasks.domain;
+
+
+@Getter
+@AllArgsConstructor
+
+public class Task {
+    private Long id;
+    private String title;
+    private String content;
+}
---
+++ tasks/src/main/java/com/crud/tasks/domain/TaskDto.java
@@ -0,0 +1,12 @@
+package com.crud.tasks.domain;
+
+
+
+@Getter
+@AllArgsConstructor
+public class TaskDto {
+    private Long id;
+    private String title;
+    private String content;
+
+}
